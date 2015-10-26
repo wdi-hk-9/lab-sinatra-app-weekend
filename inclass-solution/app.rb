82ac8cda0e5a1ded868fa7fdcddaf42c26b44118
Denis Cheung <d.s.h.cheung@gmail.com>
2015-10-26 12:17:31 +0800

    update method

--- inclass-solution/app.rb
+++ inclass-solution/app.rb
@@ -2,4 +2,34 @@ class ScotchesApp < Sinatra::Base
   get "/" do
     "Hi this is ScotchesApp"
   end
+
+  # index
+  get '/scotches' do
+    @scotches = Scotch.all
+    erb(:"scotches/index")
+  end
+
+  # new
+
+  # create
+
+  # show
+
+  # edit
+  get '/scotches/:id/edit' do
+    @scotch = Scotch.find(params[:id])
+    erb(:"scotches/edit")
+  end
+
+  # update
+  post '/scotches/:id/update' do
+    @scotch = Scotch.find(params[:id])
+    if @scotch.update_attributes(params[:scotch])
+      redirect("/scotches")
+    else
+      erb(:"scotches/edit")
+    end
+  end
+
+  # destroy
 end
